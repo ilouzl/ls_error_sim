c8c5c8bbd366f172e781dccb4a6c444461e874c1
liron ilouz <ilouzl@gmail.com>
2015-04-24 17:14:36 +0300

    SkewSymmetric bug. UpdatePosition - changed position equation.

--- UpdatePosition.m
+++ UpdatePosition.m
@@ -1,4 +1,6 @@
+%% by Woodman 2009  
 function [p v] = UpdatePosition(v_0, p_0, a_B, R_N, delta_t)
-a_N = R_N*a_B;
-v = v_0 + delta_t*(a_N+[0;0;9.81]);
-p = p_0 + delta_t*v;
+a_N = R_N*a_B+[0;0;9.81];
+v = v_0 + delta_t*a_N;
+% p = p_0 + delta_t*v; 
+p = p_0 + delta_t*v_0+0.5*a_N*delta_t^2; % not by Woodman
--- UpdateR_N.m
+++ UpdateR_N.m
@@ -1,8 +1,9 @@
+%% by Woodman 2009  
 function C = UpdateR_N(C_0,omega,delta_t)
-if delta_t == 0
+sigma = norm(delta_t*omega);
+if sigma == 0
     C = C_0;
     return;
 end
-sigma = norm(delta_t*omega);
 B = SkewSymmetric(omega)*delta_t;
 C = C_0*(eye(3) + (sin(sigma)/sigma)*B + ((1-cos(sigma))/(sigma^2))*B*B);
