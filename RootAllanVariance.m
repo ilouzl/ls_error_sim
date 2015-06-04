5bc017b1f67c9647029d59f4c9fc9730a071272d
liron ilouz <ilouzl@gmail.com>
2015-06-04 10:05:03 +0300

    Added waiter to AVAR function

--- RootAllanVariance.m
+++ RootAllanVariance.m
@@ -11,10 +11,11 @@ endLogInc = log10(maxN);
 m = unique(ceil(logspace(0,endLogInc,pts)))';  % create log spaced vector average factor
 t0 = 1/fs;                                      % t0 = sample interval
 T = m*t0;                                       % T = length of time for each cluster
+h = waitbar(0,'Calculating AVAR...');
 if ~overlapped
     %% classic AVAR according Woodman, 2009
     for i=1:length(m)
-        m(i)
+        waitbar(i/length(m),h,'Calculating AVAR...');
         N_tmp = floor(N/m(i))*m(i);
         y = x(1:N_tmp);
         y = reshape(y,m(i),N_tmp/m(i));
@@ -26,13 +27,14 @@ else
     theta  = cumsum(x)/fs;% integration of samples over time to obtain output angle ?
     sigma2 = zeros(length(T),M);% array of dimensions (cluster periods) X (#variables)
     for i=1:length(m)% loop over the various cluster sizes
-        m(i)
+        waitbar(i/length(m),h,'Calculating AVAR...');
         for k=1:N-2*m(i)% implements the summation in the AV equation
             sigma2(i,:) = sigma2(i,:) + (theta(k+2*m(i),:) - 2*theta(k+m(i),:) + theta(k,:)).^2;
         end
     end
     sigma2 = sigma2./repmat((2*T.^2.*(N-2*m)),1,M);  
 end
+close(h);
 sigma  = sqrt(sigma2);
 
 
