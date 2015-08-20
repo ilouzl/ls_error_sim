4c7585b2d5925735b0f10376369368dd0b133a46
liron ilouz <ilouzl@gmail.com>
2015-08-21 00:33:40 +0300

    INSfile outputs data in NED main file: speed and structure improvements , INS plots INS data file

--- INSfile.m
+++ INSfile.m
@@ -1,17 +1,15 @@
-function [Cbn,Cen,p_ned_delta] = INSfile(t,dt)
-persistent p flh euler v_ned p_ned;
+function [Cbn,Cen,v] = INSfile(t)
+persistent p flh euler v_ned;
 
 if isempty(p)
     load data;
     flh = x_insP(1:3,:);
     p = repmat(flh(3,:),3,1).*[repmat(cos(flh(1,:)),2,1).*[cos(flh(2,:)) ;sin(flh(2,:))]; sin(flh(1,:))];
     v_ned = x_insP(4:6,:);
-    euler = x_insP(7:9,:);
-    p_ned = 0;
+    euler = x_insP(7:9,:)*pi/180;
 end
 
 k = floor(t/0.01)+1;
 Cbn = Euler2Dcm(euler(:,k));
 Cen = Euler2Dcm(0,-pi/2-flh(1,k),flh(2,k));
-p_ned = p_ned+v_ned(:,k)*dt;
-p_ned_delta = p_ned;
+v = v_ned(:,k);
--- laserErrorSim.m
+++ laserErrorSim.m
@@ -1,22 +1,27 @@
 clearvars; close all; clc; clear INSfile.m;
 
+% load configuration and parementes
 systematic_errors;
 LS_parameters;
 surface_definition;
 platform_parameters;
 
-
+% local simulation parameters
 external_INS = 1;
+simulationLengthTime = 5; % [sec]
+simulationLengthSamples = simulationLengthTime*f_p;
 
+
+if external_INS
+    p_enu = p0_enu;
+end
+
+Cned2enu = Euler2Dcm(pi,0,pi/2);
 j = 1; % scan line
 i = 0; % laser beam within scan
 t=0;
-surface=[];
-ALS_loc = [];
-ALS_scan=[];
-e=[];
-Cned2enu = Euler2Dcm(pi,0,pi/2);
-while t<5
+[ins_euler,surface,ALS_loc,ALS_scan]=deal(zeros(3,simulationLengthSamples));
+for idx = 1:simulationLengthSamples
     i = i+1;
     if i > n
         i=1;
@@ -29,23 +34,28 @@ while t<5
         SkewSymmetric([delta_tau_i delta_phi delta_kappa]); % scan angle errors
     Cla = Euler2Dcm(tau_i,0,0);
     if external_INS
-        [Cbn,Cen,p_ned_delta] = INSfile(t,1/f_p);
-        p_enu = p0_enu+Cned2enu*p_ned_delta;
+        [Cbn,Cen,v_ned] = INSfile(t);
+        p_enu = p_enu+Cned2enu*v_ned/f_p;
     else
         Cbn = INS(t);
         Cen= Euler2Dcm(0,-pi,0);
         p_enu = GPS(t,Cned2enu*v_ned,p0_enu); % ENU
     end
-    Rtag = Cned2enu*Cbn*Cab*Cla;          % from instantaneous beam direction to ENU
-    ALS_loc = [ALS_loc p_enu];            % ENU
+    Rtag = Cned2enu*Cbn*Cab*Cla;          % from instantaneous beam direction to ENU  
     c = Cned2enu*Cbn*t_LG+p_enu;
-    [p s] = GetTrueFootprint(Rtag,c,surfaceDefinition);
-    surface = [surface p];                % ENU
-    pstar = Cned2enu*delta_Cbn*Cbn*(delta_Cab*Cab*delta_Cla*Cla*[0;0;(s+delta_r)]+t_LG+delta_t_LG)+p_enu;
-    ALS_scan = [ALS_scan pstar];
+    [p, s] = GetTrueFootprint(Rtag,c,surfaceDefinition);  % real surface
+    pstar = Cned2enu*delta_Cbn*Cbn*(delta_Cab*Cab*delta_Cla*Cla*[0;0;(s+delta_r)]+t_LG+delta_t_LG)+p_enu; % measured surface
+        
+    ALS_loc(:,idx) = p_enu;            % ENU
+    surface(:,idx) = p;                % ENU
+    ALS_scan(:,idx) = pstar;
+    ins_euler(:,idx) = Dcm2Euler(Cbn);
+    ins_v_ned(:,idx) = v_ned;
 end
+
 ALS_err = ALS_scan-surface;
 figure(1);
+title('Scene');
 scatter3(surface(1,:),surface(2,:),surface(3,:),'.');hold on;
 plot3(ALS_loc(1,:),ALS_loc(2,:),ALS_loc(3,:),'g');
 scatter3(ALS_scan(1,:),ALS_scan(2,:),ALS_scan(3,:),'r','.');
@@ -53,8 +63,25 @@ xlabel('E'); ylabel('N'); zlabel('U');
 legend({'surface','trajectory', 'scan'}); 
 
 figure(2);
+title('ALS scan');
 Scatter2Surf(ALS_scan(1,:),ALS_scan(2,:),ALS_scan(3,:));
 hold on;
 Scatter2Surf(surface(1,:),surface(2,:),surface(3,:));
 xlabel('E'); ylabel('N'); zlabel('U');
 legend({'scan','surface'}); 
+
+figure(3);
+title('Platform performance');
+t = 0:1/f_p:(simulationLengthTime-1/f_p);
+subplot(2,2,1:2);
+plot3(ALS_loc(1,:),ALS_loc(2,:),ALS_loc(3,:),'g');
+xlabel('E'); ylabel('N'); zlabel('U');
+legend({'trajectory'}); 
+subplot(2,2,3);
+plot(t,ins_euler'*180/pi); 
+legend({'\phi','\theta','\psi'}); 
+xlabel('sec'); ylabel('deg');
+subplot(2,2,4);
+plot(t,ins_v_ned'); legend({'V_N','V_E','V_D'}); 
+xlabel('sec'); ylabel('m/s');
+
