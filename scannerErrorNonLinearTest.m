c68a10df6d24fc17efbd0be68e597653e5d9a0db
Liron Ilouz <Liron@lirons-MacBook-Pro.local>
2016-01-15 12:05:54 +0200

    Non linear recovering only phi

--- scannerErrorNonLinearTest.m
+++ scannerErrorNonLinearTest.m
@@ -8,14 +8,20 @@ fs = 5000;      % laser scan rate
 T = 0.1;        % simulation period
 t = 0:1/fs:T;   % simulation time series
 x = repmat(x0,1,length(t)) + repmat(v,1,length(t)).*repmat(t,3,1);  % trajectory
-rho = (s(1:3)*x + s(4))/(s(1)*phi+s(3)); % range = ([a,b,c]*x + d)/(b*phi+c)  . from geolocation equation
+rho = (s(1:3)*x + s(4))/(s(1)*sin(phi)+s(3)*cos(phi)); % range = ([a,b,c]*x + d)/(b*phi+c)  . from geolocation equation
 % rho = (s(1:3)*x + s(4))/(-s(2)*omega+s(3));
 plot(x(1,:),rho);
 
-% h = x0(3)-x(1,:);
-% rho = h/(sin(pi/90)/tan(pi/4)+cos(pi/90)); % calculate rho by regular trigonometry
 % hold on; plot(x(1,:),rho);
-phi = (s(1:3)*x + s(4)-s(3)*rho)./(s(1)*rho);
+phi = 0;
+w = (s(1:3)*x + s(4))';
+dphi = 1;
+while abs(dphi) > 0.00001
+    W = w-(rho*(s(1)*sin(phi) + s(3)*cos(phi)))';
+    A = (rho*(s(1)*cos(phi) - s(3)*sin(phi)))';
+    dphi = inv(A'*A)*A'*W;
+    phi = phi + dphi
+end
 % omega = (s(1:3)*x + s(4)-s(3)*rho)./(-s(2)*rho);
 figure
 plot(phi*180/pi)
