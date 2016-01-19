86245621138d2442e73ea4698a4031de4d7dee0f
Liron Ilouz <Liron@lirons-MacBook-Pro.local>
2016-01-19 02:00:41 +0200

    recovering non linear range bias + papa + phi or omega bias (single slope)

--- scannerErrorNonLinearTest.m
+++ scannerErrorNonLinearTest.m
@@ -1,10 +1,10 @@
 close all; clc; clear all;
-delta_rho = 0.2;
-kappa = 0*pi/180;
+delta_rho = 0.5;
+kappa = 2*pi/180;
 phi = 0*pi/180;    % pitch bias [rad]
 omega = 3*pi/180; 
-alpha = 0*pi/180;
-s = [-1 1 1 0]'; % surface [a,b,c,d]. ax + by + cz + d = 0;
+alpha = 15*pi/180;
+s = [-1 -1 1 0]'; % surface [a,b,c,d]. ax + by + cz + d = 0;
 v = [90;0;0];   % platform velocity
 x0 = [0;0;1000]; % platform initial position
 fp = 5000;      % laser pulse rate
@@ -16,11 +16,14 @@ x = repmat(x0,1,length(t)) + repmat(v,1,length(t)).*repmat(t,3,1);  % trajectory
 
 Rins = eye(3);
 Rm = Euler2Dcm(omega,phi, kappa);
-Rs = eye(3);
-r = Rm*Rs*[0;0;-1];
+for i=1:length(alpha)
+    Rs = Euler2Dcm(alpha(i),0,0);
+    Rs_tag(:,i) = Rs*[0;0;-1];
+end
+r = Rm*Rs_tag;
 c = s(1:3)'*Rins;
 d = c*r; 
-rho = -(s(1:3)'*x + s(4)+d*delta_rho)/d;
+rho = -(s(1:3)'*x + s(4)+d*delta_rho)./d;
 plot(x(1,:),rho);
 y = 1;
 % hold on; plot(x(1,:),rho);
@@ -28,21 +31,20 @@ y = 1;
 while sum(abs(y)) > 0.0001 
     Rins = eye(3);
     Rm = Euler2Dcm(omega,phi, kappa);
-    Rs = eye(3);
-    r = Rm*Rs*[0;0;-1];
+    r = Rm*Rs_tag;
     c = s(1:3)'*Rins;
     d = c*r; 
-    w = -((s(1:3)'*x + s(4))+d*(rho+delta_rho))';%
-    A = [ones(length(rho),1)*d rho'*(r(2)*c(1)-r(1)*c(2)) ...
-         rho'*(r(1)*c(3)-r(3)*c(1)) rho'*(r(3)*c(2)-r(2)*c(3))];
-    A = A(:,[1 4]);
+    w = -((s(1:3)'*x + s(4))+d.*(rho+delta_rho))';%
+    A = [d' rho'.*(r(2,:)*c(1)-r(1,:)*c(2))' ...
+         rho'.*(r(1,:)*c(3)-r(3,:)*c(1))' rho'.*(r(3,:)*c(2)-r(2,:)*c(3))'];
+    A = A(:,[1 2 4]);
     y = inv(A'*A)*A'*w;
     delta_rho = delta_rho + y(1)
-%     kappa = kappa + y(2);
-%     kappa*180/pi
-%     phi = phi + y(2);
+    kappa = kappa + y(2);
+    kappa*180/pi
+%     phi = phi + y(3);
 %     phi*180/pi
-    omega = omega + y(2);
+    omega = omega + y(3);
     omega*180/pi
 end
 figure
